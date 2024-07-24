ad1772244ccd652715663b9dd8ebe7549e9ae5ee
jjhiggz <jonathan.higger@gmail.com>
2024-07-24 17:54:05 -0600

    a user can now see their checklist

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { prisma } from "../prisma/prisma";
 
 const app = new Hono();
-
+app.use("/*", cors());
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
